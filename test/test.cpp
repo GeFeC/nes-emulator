f968d119a005ce089716a57dc70be104bed83735
GeFeC <poucorron@gmail.com>
2025-08-11 13:43:19 +0200

    Update: Changed test to print values in hex in error messages

--- test/test.cpp
+++ test/test.cpp
@@ -7,7 +7,7 @@
 namespace nes{
 
 struct LogData{
-  std::string cmd, a, x, y, p, sp, pc;
+  u16 cmd, a, x, y, p, sp, pc;
   int cycles;
 };
 
@@ -17,17 +17,23 @@ inline auto ichar_equals(char a, char b) {
     std::tolower(static_cast<unsigned char>(b));
 }
 
-inline auto hex(const std::string& str){
+inline auto hex_str(u16 value){
+  std::stringstream ss;
+  ss << std::hex << value;
+  return ss.str();
+}
+
+inline auto from_hex_str(const std::string& str){
   std::stringstream ss;
   ss << std::hex << str;
-  u16 x;
-  ss >> x;
-  return x;
+  u16 value;
+  ss >> value;
+  return value;
 }
 
 inline auto test(const std::string& name, u16 cmds, u16 expected, u16 got){
   if (got != expected){
-    throw std::runtime_error(std::to_string(cmds + 1) + "   Expected " + name + ": " + std::to_string(expected) + " but got " + std::to_string(got));
+    throw std::runtime_error(std::to_string(cmds + 1) + "   Expected " + name + ": " + hex_str(expected) + " but got " + hex_str(got));
   }
 }
 
@@ -42,13 +48,13 @@ inline auto test_cpu(){
   auto line = std::string();
   while(std::getline(log_file, line)){
     auto& data = log_data.emplace_back();
-    data.pc = line.substr(0, 4);
-    data.cmd = line.substr(6, 2); 
-    data.a = line.substr(50, 2);
-    data.x = line.substr(55, 2);
-    data.y = line.substr(60, 2);
-    data.p = line.substr(65, 2);
-    data.sp = line.substr(71, 2);
+    data.pc = from_hex_str(line.substr(0, 4));
+    data.cmd = from_hex_str(line.substr(6, 2));
+    data.a = from_hex_str(line.substr(50, 2));
+    data.x = from_hex_str(line.substr(55, 2));
+    data.y = from_hex_str(line.substr(60, 2));
+    data.p = from_hex_str(line.substr(65, 2));
+    data.sp = from_hex_str(line.substr(71, 2));
     data.cycles = std::stoi(line.substr(90));
   }
 
@@ -61,13 +67,13 @@ inline auto test_cpu(){
     const auto& data = log_data[cmds];
 
     auto& cpu = nes.cpu;
-    test("PC", cmds, hex(data.pc), cpu.pc);
-    test("CMD", cmds, hex(data.cmd), (nes.mem_read(cpu.pc)));
-    test("A", cmds, hex(data.a), (cpu.accumulator));
-    test("X", cmds, hex(data.x), (cpu.x));
-    test("Y", cmds, hex(data.y), (cpu.y));
-    test("P", cmds, hex(data.p), (cpu.status));
-    test("SP", cmds, hex(data.sp), (cpu.sp));
+    test("PC", cmds, data.pc, cpu.pc);
+    test("CMD", cmds, data.cmd, (nes.mem_read(cpu.pc)));
+    test("A", cmds, data.a, (cpu.accumulator));
+    test("X", cmds, data.x, (cpu.x));
+    test("Y", cmds, data.y, (cpu.y));
+    test("P", cmds, data.p, (cpu.status));
+    test("SP", cmds, data.sp, (cpu.sp));
     test("CYC", cmds, data.cycles, cpu.cycles);
 
     try{
